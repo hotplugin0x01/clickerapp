c6ed23a255adf66f4421eedd6256471f93235151
wasay <wasay@hackyy.local>
2024-12-03 00:30:46 +0500

    Add internet functionality - Code in MainActivity - Permission in Manifest file

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,6 +13,17 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -33,15 +44,38 @@ public class MainActivity extends AppCompatActivity {
         clickme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                counter++;
-                homeText.setText(String.format(
-                        getString(R.string.counter_text)   , counter
-                ));
-                Log.i("Logging: ", String.format("The Button is clicked!: Counter: %d", counter));
-                if (counter >= 10){
-//                    Intent browser = new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com/"));
-                    Intent myIntent = new Intent(MainActivity.this, SecretActivity.class);
-                    startActivity(myIntent);
+//                counter++;
+//                homeText.setText(String.format(
+//                        getString(R.string.counter_text)   , counter
+//                ));
+//                Log.i("Logging: ", String.format("The Button is clicked!: Counter: %d", counter));
+//                if (counter >= 10){
+////                    Intent browser = new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com/"));
+//                    Intent myIntent = new Intent(MainActivity.this, SecretActivity.class);
+//                    startActivity(myIntent);
+//                }
+                // Internet Connection Code
+                try {
+                    ExecutorService executor = Executors.newSingleThreadExecutor();
+                    executor.execute( () -> {
+                        try {
+                            URL url = new URL("http://www.google.com/");
+                            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                            StringBuilder sb = new StringBuilder();
+                            String line;
+                            while ((line = reader.readLine()) != null) {
+                                sb.append(line).append('\n');
+                            }
+                            String result = sb.toString();
+                            runOnUiThread(() -> homeText.setText(result));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         });
